4efd59b9ee6d2a8f21cfe51ecd626e2cacb85016
Jeremy Perrin <japerr@japerr.com>
2018-05-02 13:02:03 -0500

    Auto build version update

--- app/include/user_version.h
+++ app/include/user_version.h
@@ -11,10 +11,10 @@
 #define NODE_VERSION_STR(x)	#x
 #define NODE_VERSION_XSTR(x)	NODE_VERSION_STR(x)
 
-#define NODE_VERSION		"NodeMCU " ESP_SDK_VERSION_STRING "." NODE_VERSION_XSTR(NODE_VERSION_INTERNAL)
+#define NODE_VERSION		"NodeMCU " ESP_SDK_VERSION_STRING "." NODE_VERSION_XSTR(NODE_VERSION_INTERNAL) " built with Docker provided by frightanic.com\n\tbranch: master\n\tcommit: 67027c0d05f7e8d1b97104e05a3715f6ebc8d07f\n\tSSL: false\n\tmodules: adc,bit,dht,file,gpio,i2c,mqtt,net,node,ow,spi,tls,tmr,uart,wifi\n"
 
 #ifndef BUILD_DATE
-#define BUILD_DATE		"unspecified"
+#define BUILD_DATE		"created on 2018-05-02 17:11\n"
 #endif
 
 extern char SDK_VERSION[];
